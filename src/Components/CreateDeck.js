a344d388e4adf1239b53a3576ae5f67b8bcab9b6
JohnnyChen129 <copperfists@gmail.com>
2022-03-29 10:20:02 -0400

    fixed error

--- src/Components/CreateDeck.js
+++ src/Components/CreateDeck.js
@@ -3,6 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import { createDeck } from "../utils/api/index";
 
 function CreateDeck() {
+    
     const history = useHistory();
     const initialState = {
         name: "",
