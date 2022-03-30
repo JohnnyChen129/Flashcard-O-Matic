26d23af353e85440fdeb4a7cd71fa6487238470a
JohnnyChen129 <copperfists@gmail.com>
2022-03-30 12:06:24 -0400

    hello

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,46 +1,49 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { Fragment } from "react";
+import { Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import NotFound from "./NotFound";
-import Home from "../Components/Home";
-import CreateDeck from "../Components/CreateDeck";
-import Deck from "../Components/Deck";
-import Study from "../Components/Study";
-import EditDeck from "../Components/EditDeck";
-import AddCard from "../Components/AddCard";
-import EditCard from "../Components/EditCard";
+import CreateDeck from "../Deck/CreateDeck";
+import Home from "../Home/Home";
+import Study from "../Deck/Study";
+import ViewDeck from "../Deck/ViewDeck";
+import EditDeck from "../Deck/EditDeck";
+import EditCard from "../Cards/EditCard";
+import AddCard from "../Cards/AddCard";
+
 function Layout() {
-    return (
-        <div>
-            <Header />
-            <div className="container">
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Route path="/decks/new">
-                        <CreateDeck />
-                    </Route>
-                    <Route exact path="/decks/:deckId">
-                        <Deck />
-                    </Route>
-                    <Route path="/decks/:deckId/study">
-                        <Study />
-                    </Route>
-                    <Route path="/decks/:deckId/edit">
-                        <EditDeck />
-                    </Route>
-                    <Route path="/decks/:deckId/cards/new">
-                        <AddCard />
-                    </Route>
-                    <Route path="/decks/:deckId/cards/:cardId/edit">
-                        <EditCard />
-                    </Route>
-                    <NotFound />
-                </Switch>
-            </div>
-        </div>
-    );
+  return (
+    <Fragment>
+      <Header />
+      <div className="container">
+        <Switch>
+          <Route exact path={"/"}>
+            <Home />
+          </Route>
+          <Route exact path={"/decks/new"}>
+            <CreateDeck />
+          </Route>
+          <Route exact path={"/decks/:deckId"}>
+            <ViewDeck />
+          </Route>
+          <Route exact path={"/decks/:deckId/edit"}>
+            <EditDeck />
+          </Route>
+          <Route exact path={"/decks/:deckId/study"}>
+            <Study />
+          </Route>
+          <Route exact path={"/decks/:deckId/cards/new"}>
+            <AddCard />
+          </Route>
+          <Route exact path={"/decks/:deckId/cards/:cardId/edit"}>
+            <EditCard />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </div>
+    </Fragment>
+  );
 }
 
 export default Layout;
